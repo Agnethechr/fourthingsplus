be40906c874e80bb51227a22d5791b7bf3b423c5
malouVich <malouvichlavrentiew@gmail.com>
2024-10-03 14:46:49 +0200

    menucontroller

--- app-demo/src/main/java/app/controllers/MenuController.java
+++ app-demo/src/main/java/app/controllers/MenuController.java
@@ -13,18 +13,32 @@ import java.util.List;
 
 public class MenuController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.get("/task", ctx -> menus(ctx, connectionPool));
+        app.get("/list", ctx -> menus(ctx, connectionPool));
     }
 
-    private static void menus(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
-        User user = ctx.sessionAttribute("currentUser");
+    //private static void menus(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+//        User user = ctx.sessionAttribute("currentUser");
+//
+//        List<Lists> menu = ListMapper.getAllListsPerUser(user.getUserId(), connectionPool);
+//        ArrayList<String> nameMenu = new ArrayList<>();
+//        for (Lists name : menu){
+//            nameMenu.add(name.getName());
+//        }
+//
+//        System.out.println(nameMenu);
+//
+//
+//        ctx.attribute("nameMenu", nameMenu);
+//        ctx.render("list.html");
+        private static void menus(Context ctx, ConnectionPool connectionPool){
+            ArrayList<String> menu = new ArrayList<>();
+            menu.add("forside");
+            menu.add("andet");
+            menu.add("trejde");
 
-        List<Lists> menu = new ArrayList<>();
-        menu = ListMapper.getAllListsPerUser(user.getUserId(), connectionPool);
+            ctx.attribute("menu", menu);
+            ctx.render("task.html");
 
-
-        ctx.attribute("menu", menu);
-        ctx.render("task.html");
-
+        }
     }
-}
+
